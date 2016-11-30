d36032c817c484d1a03f7395978f604349ee3d07
Adam Nemitoff <adamn@teamnemitoff.com>
2016-11-30 17:08:03 -0500

    Fix broken pull request
    That will teach me to accept a pull request without reviewing it
    thoroughly.

--- src/android/PhoneDialer.java
+++ src/android/PhoneDialer.java
@@ -6,28 +6,31 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.Context;
 import android.net.Uri;
+import android.telephony.TelephonyManager;
 
 public class PhoneDialer extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 	    try {
-	TelephonyManager telMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-    	if(tm.getSimState() != TelephonyManager.SIM_STATE_ABSENT)
-		    {
-	    	String phoneNumber = args.getString(0);
-	    	Uri uri = Uri.parse("tel:"+phoneNumber);
-            Intent callIntent = new Intent(Intent.ACTION_CALL);
-            callIntent.setData(uri);
-            this.cordova.getActivity().startActivity(callIntent);
-            callbackContext.success();
-            return true;
-		    }
+			Context context = this.cordova.getActivity().getApplicationContext(); 
+			TelephonyManager telMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			if(telMgr.getSimState() != TelephonyManager.SIM_STATE_ABSENT) {
+				String phoneNumber = args.getString(0);
+				Uri uri = Uri.parse("tel:"+phoneNumber);
+				Intent callIntent = new Intent(Intent.ACTION_CALL);
+				callIntent.setData(uri);
+				this.cordova.getActivity().startActivity(callIntent);
+				callbackContext.success();
+				return true;
+			}
         } catch (Exception e) {
         	String msg = "Exception Dialing Phone Number: " + e.getMessage();
         	System.err.println(msg);
 	        callbackContext.error(msg);
-	        return false;
         }
+		return false;
 	}
+
 }
